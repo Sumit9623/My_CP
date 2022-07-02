57e17f891846fa1451266faafd9505df75e4fba2
Sumit_Tupe <sumittupe9623@gmail.com>
2022-07-02 19:54:47 +0530

    array reordering, Bad boy 2 july 2022

---
+++ C2_Ladder/Bad_boy.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastIO                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+int main()
+{
+    fastIO;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m,i,j;
+        cin>>n>>m>>i>>j;
+        cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+    }
+    return 0;
+    
+}
