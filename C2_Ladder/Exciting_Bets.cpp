33b47a1052eb165d505e0b1048554becaf3aadfd
Sumit_Tupe <sumittupe9623@gmail.com>
2022-07-03 10:00:56 +0530

    3 july 2022

--- C2_Ladder/Exciting_Bets.cpp
+++ C2_Ladder/Exciting_Bets.cpp
@@ -17,31 +17,11 @@ int main()
         {
             cout<<0<<" "<<0<<endl;
         }
-        else if(a==0 || b==0)
+        else
         {
-            if(a>=b) cout<<a<<" "<<0<<endl;
-            else cout<<b<<" "<<0<<endl;
-        }
-        else if(a-b==1 || b-a==1) cout<<1<<" "<<0<<endl;
-        else if(a>b) 
-        {
-            cout<<a-b<<" ";
-            if(b>(a-b))
-            {
-                if(a%(a-b)>((a-b)-a%(a-b))) cout<<((a-b)-a%(a-b))<<endl;
-            }
-            else if(b==(a-b)) cout<<0<<endl;
-            else cout<<b<<endl;
-        }
-        else 
-        {
-            cout<<b-a<<" ";
-            if (a>(b-a))
-            {
-                if(b%(b-a)>((b-a)-b%(b-a))) cout<<((b-a)-b%(b-a))<<endl;
-            }
-            else if(a==(b-a)) cout<<0<<endl;
-            else cout<<a<<endl;
+            long long g = abs(a-b);
+            long long m = min(a%g,g-a%g);
+            cout << g << " " << m << '\n';
         }
      
     }
